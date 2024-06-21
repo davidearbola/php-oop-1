ba968ed23a255ae5dcd0e2d59a4ef6741d6aad8d
davidearbola <davidearbola@gmail.com>
2024-06-21 18:57:25 +0200

    creo classe Movie e creo variabili di istanza all'interno di Movie, creo nuovi Movie e li stampo in pagina

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php
 
+class Movie
+{
+    public $titolo;
+    public $descrizione;
+    public $lingua;
+    public $durata;
+}
+$fastAndFurios = new Movie();
+$fastAndFurios->titolo = 'Fast & Furious';
+$fastAndFurios->descrizione = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, quam!';
+$fastAndFurios->lingua = 'EN';
+$fastAndFurios->durata = '120 minuti';
 
+$ilPadrino = new Movie();
+$ilPadrino->titolo = 'Il Padrino';
+$ilPadrino->descrizione = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, quam!';
+$ilPadrino->lingua = 'EN';
+$ilPadrino->durata = '180 minuti';
+
+$gomorra = new Movie();
+$gomorra->titolo = 'Gomorra';
+$gomorra->descrizione = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, quam!';
+$gomorra->lingua = 'IT';
+$gomorra->durata = '100 minuti';
 
 ?>
 
@@ -15,7 +38,19 @@
 </head>
 
 <body>
-    <h1>ciao</h1>
+
+    <h3><?= "Titolo: $fastAndFurios->titolo" ?></h3>
+    <p><?= "Descrizione: $fastAndFurios->descrizione" ?></p>
+    <span><?= "Lingua: $fastAndFurios->lingua | Durata: $fastAndFurios->durata" ?></span>
+
+    <h3><?= "Titolo: $ilPadrino->titolo" ?></h3>
+    <p><?= "Descrizione: $ilPadrino->descrizione" ?></p>
+    <span><?= "Lingua: $ilPadrino->lingua | Durata: $ilPadrino->durata" ?></span>
+
+    <h3><?= "Titolo: $gomorra->titolo" ?></h3>
+    <p><?= "Descrizione: $gomorra->descrizione" ?></p>
+    <span><?= "Lingua: $gomorra->lingua | Durata: $gomorra->durata" ?></span>
+
 </body>
 
 </html>
